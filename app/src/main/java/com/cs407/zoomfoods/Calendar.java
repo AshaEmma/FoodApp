60462286a4982dc8139c938c7a6db3e6dfdd82c8
AshaEmma <aawright3@wisc.edu>
2023-12-04 10:37:15 -0600

    Registration and Login classes & xml files created everything is in working order

--- app/src/main/java/com/cs407/zoomfoods/Calendar.java
+++ app/src/main/java/com/cs407/zoomfoods/Calendar.java
@@ -1,14 +1,37 @@
 package com.cs407.zoomfoods;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
-
+    CalendarView calendarView;
+    TextView dateView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendar); //getting reference to calendar view
+        dateView = (TextView) findViewById(R.id.dateView);
+
+        //Add listener
+        calendarView
+                .setOnDateChangeListener(
+                        new CalendarView.OnDateChangeListener() {
+                            @Override
+                            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                                //Store date
+                                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+
+                                //display date in textView
+                                dateView.setText(date);
+                            }
+                        }
+                );
     }
 }
--- app/src/main/java/com/cs407/zoomfoods/MainActivity.java
+++ app/src/main/java/com/cs407/zoomfoods/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
+    private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("APP_STARTED", "====== App Started");
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+        signUp = findViewById(R.id.signup);
     }
 }
