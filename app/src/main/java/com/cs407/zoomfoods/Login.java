101d7eca3dc44c637012aa7da336f2e150d78314
AshaEmma <aawright3@wisc.edu>
2023-12-04 17:52:44 -0600

    Added navigation to Dashboard, Login, Profile, and Registration. Updated dashboard and registration xml UI

--- app/src/main/java/com/cs407/zoomfoods/Login.java
+++ app/src/main/java/com/cs407/zoomfoods/Login.java
@@ -1,8 +1,12 @@
 package com.cs407.zoomfoods;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = findViewById(R.id.btn_login);
+        Button newUser= findViewById(R.id.btn_newUser);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        newUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRegistrationActivity();
+            }
+        });
+    }
+
+    public void login(){
+     //TODO: Handle Login
+        Intent loginIntent = new Intent(Login.this, Dashboard.class);
+        startActivity(loginIntent);
+    }
+
+    public void keepMeLogged(){
+        //TODO: Handle Keep me Logged in
+    }
+
+    public void openRegistrationActivity(){
+        Intent signupIntent = new Intent(Login.this, Registration.class);
+        startActivity(signupIntent);
     }
 }
--- app/src/main/java/com/cs407/zoomfoods/Profile.java
+++ app/src/main/java/com/cs407/zoomfoods/Profile.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
-
+//TODO: add menu to navigate back to places
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
