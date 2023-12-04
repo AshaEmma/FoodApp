e59f85c3da850c939571f4b07a3695d71b76265d
AshaEmma <aawright3@wisc.edu>
2023-12-04 16:33:33 -0600

    Added dashboard, profile, .xml files and .java files. Updated strings; manifest, colors, and added Intents to main activity

--- app/src/main/java/com/cs407/zoomfoods/MainActivity.java
+++ app/src/main/java/com/cs407/zoomfoods/MainActivity.java
@@ -2,20 +2,48 @@ package com.cs407.zoomfoods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button login;
-    private Button signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i("APP_STARTED", "====== App Started");
         setContentView(R.layout.activity_main);
 
-        login = findViewById(R.id.login);
-        signUp = findViewById(R.id.signup);
+        Button login = findViewById(R.id.login);
+        Button signUp = findViewById(R.id.signup);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRegistrationActivity();
+            }
+        });
+
+
     }
+    public void openLoginActivity(){
+        Intent loginIntent = new Intent(MainActivity.this, Login.class);
+        startActivity(loginIntent);
+    }
+
+    public void openRegistrationActivity(){
+        Intent signupIntent = new Intent(MainActivity.this, Registration.class);
+        startActivity(signupIntent);
+    }
+
 }
