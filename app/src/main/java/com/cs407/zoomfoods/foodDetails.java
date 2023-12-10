a47e56c725a78d369276310c815674dd87606d5e
vazhan <145057549+vazhan@users.noreply.github.com>
2023-12-10 11:32:42 -0600

    Added food tracking layouts, and associated implementation and databases

---
+++ app/src/main/java/com/cs407/zoomfoods/foodDetails.java
@@ -0,0 +1,160 @@
+package com.cs407.zoomfoods;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.cs407.zoomfoods.utils.Constants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+
+public class foodDetails extends AppCompatActivity {
+
+    private TextView itemNameTextView;
+    private TextView itemGrams;
+    private EditText gramsText;
+    private String itemName;
+    private String calories;
+    private String protein;
+    private String carbs;
+    private String fat;
+    private String sodium;
+    private String potassium;
+    private String meal;
+    private double grams;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fooddetails);
+        Button submitButton = findViewById(R.id.submitButton);
+        Button editButton = findViewById(R.id.editButton);
+        editButton.setVisibility(View.INVISIBLE);
+        submitButton.setVisibility(View.INVISIBLE);
+
+        itemNameTextView = findViewById(R.id.indItemName);
+        gramsText = findViewById(R.id.gramsEdit);
+    gramsText.setVisibility(View.INVISIBLE);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            if (intent.getStringExtra("class").equals("foodSearch")) {
+                gramsText.setVisibility(View.VISIBLE);
+                itemName = intent.getStringExtra("itemName");
+                calories = intent.getStringExtra("calories");
+                protein = intent.getStringExtra("protein");
+                carbs = intent.getStringExtra("carbs");
+                fat = intent.getStringExtra("fat");
+                sodium = intent.getStringExtra("sodium");
+                potassium = intent.getStringExtra("potassium");
+                meal = intent.getStringExtra("meal");
+
+                itemNameTextView.setText(itemName);
+                grams = 1;
+            }
+            else {
+                editButton.setVisibility(View.VISIBLE);
+                itemName = intent.getStringExtra("itemName");
+                calories = intent.getStringExtra("calories");
+                protein = intent.getStringExtra("protein");
+                carbs = intent.getStringExtra("carbs");
+                fat = intent.getStringExtra("fat");
+                sodium = intent.getStringExtra("sodium");
+                potassium = intent.getStringExtra("potassium");
+                meal = intent.getStringExtra("meal");
+                grams = Double.parseDouble(intent.getStringExtra("amount"));
+                itemGrams = findViewById(R.id.itemGrams);
+                itemGrams.setText("Amount in grams: " + String.format("%.1f", grams * 100));
+
+
+                itemNameTextView.setText(itemName);
+            }
+            changeText();
+        }
+
+        gramsText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                submitButton.setVisibility(View.VISIBLE);
+                if (!gramsText.getText().toString().isEmpty()) {
+                    grams = (Double.parseDouble(gramsText.getText().toString())) / 100;
+                }
+                changeText();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        submitButton.setOnClickListener(this::submitGrams);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitButton.setVisibility(View.VISIBLE);
+                gramsText.setVisibility(View.VISIBLE);
+                itemGrams.setText("Amount in grams:");
+            }
+        });
+    }
+
+    public void submitGrams(View view) {
+        Context context = getApplicationContext();
+        SQLiteDatabase db = context.openOrCreateDatabase("Zoom Foods Database", Context.MODE_PRIVATE, null);
+        foodLogTable log = new foodLogTable(db);
+        log.createTable(context);
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+        String formattedDate = dateFormat.format(date);
+
+        log.addFood(Integer.parseInt(Constants.USER_ID), formattedDate, meal, itemName, grams, Double.parseDouble(calories) * grams, Double.parseDouble(protein) * grams,
+                Double.parseDouble(carbs) * grams, Double.parseDouble(fat) * grams, Double.parseDouble(sodium) * grams, Double.parseDouble(potassium) * grams);
+        db.close();
+
+        Intent intent = new Intent(foodDetails.this, foodTracking.class);
+        startActivity(intent);
+    }
+
+    public void changeText() {
+        TextView mealText = findViewById(R.id.meal_TextView);
+        TextView caloriesText = findViewById(R.id.calories_TextView);
+        TextView proteinText = findViewById(R.id.protein_TextView);
+        TextView carbsText = findViewById(R.id.carbs_TextView);
+        TextView fatText = findViewById(R.id.fat_TextView);
+        TextView sodiumText = findViewById(R.id.sodium_TextView);
+        TextView potassiumText = findViewById(R.id.potassium_TextView);
+
+        itemNameTextView.setText(itemName);
+        String mealString = "Meal: " + meal;
+        String caloriesString = "Calories: " + String.format("%.1f", Double.parseDouble(calories) * grams);
+        String proteinString = "Protein: " + String.format("%.1f", Double.parseDouble(protein) * grams);
+        String carbsString = "Carbohydrates: " + String.format("%.1f", Double.parseDouble(carbs) * grams);
+        String fatString = "Fat: " + String.format("%.1f", Double.parseDouble(fat) * grams);
+        String sodiumString = "Sodium: " + String.format("%.1f", Double.parseDouble(sodium) * grams);
+        String potassiumString = "Potassium: " + String.format("%.1f", Double.parseDouble(potassium) * grams);
+        mealText.setText(mealString);
+        caloriesText.setText(caloriesString);
+        proteinText.setText(proteinString);
+        carbsText.setText(carbsString);
+        fatText.setText(fatString);
+        sodiumText.setText(sodiumString);
+        potassiumText.setText(potassiumString);
+    }
+}
---
+++ app/src/main/java/com/cs407/zoomfoods/foodNutritionTable.java
@@ -0,0 +1,74 @@
+package com.cs407.zoomfoods;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class foodNutritionTable {
+    private static final String databaseName = "Foods Database";
+    static SQLiteDatabase database;
+    public foodNutritionTable(SQLiteDatabase sqLiteDatabase)
+    {
+        this.database = sqLiteDatabase;
+    }
+
+    private static final String CREATE_TABLE_FOODNUTRITION = "CREATE TABLE IF NOT EXISTS foodNutrition ("
+            + "name TEXT,"
+            + "calories REAL,"
+            + "protein REAL,"
+            + "fat REAL,"
+            + "carbs REAL,"
+            + "sodium REAL,"
+            + "potassium REAL);";
+
+
+    public static void createTable(Context context) {
+        SQLiteDatabase db = context.openOrCreateDatabase(databaseName, Context.MODE_PRIVATE, null);
+        db.execSQL("DROP TABLE IF EXISTS foodNutrition");
+        db.execSQL(CREATE_TABLE_FOODNUTRITION);
+        db.close();
+    }
+
+    public void importData(Context context, String filePath) {
+        try {
+            InputStream inputStream = context.getAssets().open(filePath);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split("\\|");
+                String name = values[0].trim();
+                double calories = Double.parseDouble(values[1].trim());
+                double protein = Double.parseDouble(values[2].trim());
+                double carbohydrates = Double.parseDouble(values[3].trim());
+                double fat = Double.parseDouble(values[4].trim());
+                double sodium = Double.parseDouble(values[5].trim());
+                double potassium = Double.parseDouble(values[6].trim());
+
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("name", name);
+                contentValues.put("calories", calories);
+                contentValues.put("protein", protein);
+                contentValues.put("carbs", carbohydrates);
+                contentValues.put("fat", fat);
+                contentValues.put("sodium", sodium);
+                contentValues.put("potassium", potassium);
+
+                database.insert("foodNutrition", null, contentValues);
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            database.close();
+        }
+    }
+}
