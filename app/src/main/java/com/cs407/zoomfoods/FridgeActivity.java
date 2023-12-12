80c579647ead2ac124fa8b6f89cb6979c9cf22b7
AshaEmma <aawright3@wisc.edu>
2023-12-12 11:23:49 -0600

    added fridge class and xml files

---
+++ app/src/main/java/com/cs407/zoomfoods/FridgeActivity.java
@@ -0,0 +1,15 @@
+package com.cs407.zoomfoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class FridgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge);
+    }
+}
